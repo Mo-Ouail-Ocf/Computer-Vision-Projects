0eaee5f6030c1792d0d8490e64cb1928522707bd
Ouail Oucherif <m.ouail.oucherif@gmail.com>
2024-08-21 15:01:33 +0100

    added data & model for GANs

---
+++ image_generation/GANs/data.py
@@ -0,0 +1,82 @@
+import pathlib
+import cv2
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+import typing as tt
+import torch
+from PIL import Image
+CURR_PATH = pathlib.Path(__file__).parent
+
+DATA_PATH_F = CURR_PATH / 'females'
+DATA_PATH_M = CURR_PATH / 'males'
+CROPPED_IMGS_PATH = CURR_PATH / 'cropped'
+
+def create_cropped_dataset(cropped_imgs_path: pathlib.Path):
+
+    cropped_files : tt.List[str] = []
+    cropped_imgs_path.mkdir(parents=True, exist_ok=True)
+
+    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    f_files = [f for f in DATA_PATH_F.glob('*.jpg')]
+    m_files = [f for f in DATA_PATH_M.glob('*.jpg')]
+    files = f_files + m_files
+    
+    for i, file_path in enumerate(files):
+        if i == 100:  # Limit to 100 images
+            break
+        
+        img = cv2.imread(str(file_path))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
+        )
+
+        for j, (x, y, w, h) in enumerate(faces):
+            cropped_img = img[y:y+h, x:x+w]
+            output_path = cropped_imgs_path / f'{i}_{j}.jpg'
+            cv2.imwrite(str(output_path), cropped_img)
+            cropped_files.append(str(output_path))
+    return cropped_files
+
+cropped_files=create_cropped_dataset(CROPPED_IMGS_PATH)
+
+transform = transforms.Compose([
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=(0.5,0.5,0.5),std=(0.5,0.5,0.5)
+    ),
+])
+
+'''
+Considerations for the Order
+-Spatial Transformations: Operations like resizing, cropping, and flipping should generally be performed before 
+ converting the image to a tensor (ToTensor()), as these operations are more intuitive and efficient to perform on PIL images.
+-Pixel Value Transformations: Conversions like normalization should occur after the image has been converted to a tensor, as these 
+ operations are mathematically defined on tensors.
+'''
+
+class FacesDataset(Dataset):
+    def __init__(self,transform : transforms.Compose,file_names:tt.List[str],device='cuda'):
+        self.transform = transform
+        self.file_names = file_names
+        self.device=device
+
+    def __len__(self): 
+        return len(self.file_names)
+
+    def __getitem__(self, index)->torch.Tensor:
+        img_file = self.file_names[index]
+        img = Image.open(img_file)
+        img_t:torch.Tensor = self.transform(img)
+        return img_t.to(self.device)
+    
+
+face_dataset = FacesDataset(transform,cropped_files)
+
+face_dl = DataLoader(face_dataset,shuffle=True,batch_size=64)
+
+
+if __name__=="__main__":
+    print(cropped_files[-1])
---
+++ image_generation/GANs/model.py
@@ -0,0 +1,130 @@
+import torch.nn as nn
+from torchsummary import summary
+import torch
+
+# Proper init according to the paper
+def weight_init_gan(layer:nn.Module):
+    if isinstance(layer,nn.Conv2d) or isinstance(layer,nn.ConvTranspose2d):
+        nn.init.normal_(layer.weight.data,0,0.02)
+    
+    if isinstance(layer,nn.BatchNorm2d): 
+        nn.init.normal_(layer.weight.data,1.0,0.02)
+        nn.init.constant_(layer.bias.data, 0)
+
+    
+
+class Discriminator(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.model=nn.Sequential(
+            nn.Conv2d(3,64,4,2,1,bias=False),
+            nn.LeakyReLU(0.2,inplace=True),
+
+            nn.Conv2d(64,64*2,4,2,1),
+            nn.BatchNorm2d(64*2),
+            nn.LeakyReLU(0.2,inplace=True),
+            
+            nn.Conv2d(64*2,64*4,4,2,1,bias=False),
+            nn.BatchNorm2d(64*4),
+            nn.LeakyReLU(0.2,inplace=True),
+
+            nn.Conv2d(64*4,64*8,4,2,1,bias=False),
+            nn.BatchNorm2d(64*8),
+            nn.LeakyReLU(0.2,inplace=True),
+
+            nn.Conv2d(64*8,1,4,1,0,bias=False),
+            nn.Sigmoid()
+
+        )
+
+        self.apply(weight_init_gan)
+
+    def forward(self,x)->torch.Tensor:
+        return self.model(x)
+    
+class Generator(nn.Module):
+     def __init__(self) -> None:
+        super().__init__()
+        
+        self.model=nn.Sequential(
+            nn.ConvTranspose2d(100,64*8,4,1,0,bias=False,),
+            nn.BatchNorm2d(64*8),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(64*8,64*4,4,2,1,bias=False),
+            nn.BatchNorm2d(64*4),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d( 64*4,64*2,4,2,1,bias=False),
+            nn.BatchNorm2d(64*2),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d( 64*2,64,4,2,1,bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d( 64,3,4,2,1,bias=False),
+            nn.Tanh()
+        )
+        self.apply(weight_init_gan)
+     def forward(self,x)->torch.Tensor:
+        return self.model(x)
+
+def get_gen_disc()->tuple[Generator, torch.optim.Adam, Discriminator, torch.optim.Adam, nn.BCELoss]:
+    model_gen = Generator().to('cuda')
+    optim_gen = torch.optim.Adam(params=model_gen.parameters(),lr=0.0002, betas=(0.5, 0.999))
+    model_disc = Discriminator().to('cuda')
+    optim_disc =  torch.optim.Adam(params=model_disc.parameters(),lr=0.0002, betas=(0.5, 0.999))
+    loss_fn = nn.BCELoss()
+    return model_gen,optim_gen,model_disc,optim_disc,loss_fn
+
+
+def calc_disc_loss(model_disc:Discriminator,loss_fn,batch:torch.Tensor,targets:torch.Tensor):
+    preds = model_disc(batch)
+    assert isinstance(preds,torch.Tensor)
+    preds = preds.squeeze()
+    return loss_fn(preds,targets)
+
+
+def disc_train_step(model_disc:Discriminator,loss_fn,real_data_batch :torch.Tensor, fake_data_batch:torch.Tensor):
+    model_disc.train()
+    len_real = real_data_batch.shape[0]
+    targets_real = torch.ones(len_real)
+
+    len_fake = fake_data_batch.shape[0]
+    targets_fake = torch.zeros(len_fake)
+
+    loss_real , loss_fake = calc_disc_loss(model_disc,loss_fn,real_data_batch,targets_real),\
+                         calc_disc_loss(model_disc,loss_fn,fake_data_batch,targets_fake)
+    
+    return loss_fake+loss_real
+
+def get_synth_images(model_gen:Generator,nb_images:int,train=False,device='cuda')->torch.Tensor:
+    if train:
+        model_gen.train()
+        z = torch.randn(size=(nb_images,100,1,1)).to(device)
+        return model_gen(z)
+    else:
+        model_gen.eval()
+        with torch.no_grad():
+            z = torch.randn(size=(nb_images,100,1,1)).to(device)
+            return model_gen(z)
+
+def gen_train_step(model_gen:Generator,model_disc:Discriminator,nb_images:int,loss_fn):
+    synth_imgs = get_synth_images(model_gen,nb_images,train=True)
+
+    # get preds
+    targets_fake = torch.ones(nb_images)
+
+    model_disc.eval()
+    
+    loss = calc_disc_loss(model_disc,loss_fn,synth_imgs,targets_fake)
+
+    return loss
+
+
+if __name__=="__main__":
+    model_disc = Discriminator().to('cuda')
+    model_gen = Generator().to('cuda')
+    print(summary(model_disc,input_size=(3,64,64)))
